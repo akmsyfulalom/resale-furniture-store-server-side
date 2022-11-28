cf3d1c7acc8b9e315238dfb98d530417284dd906
akmsyfulalom <akmsyfulbd12@gmail.com>
2022-11-28 08:58:39 +0600

    product delete api implement

--- index.js
+++ index.js
@@ -54,6 +54,12 @@ async function run() {
             const query = { email: email }
             const products = await productsCollection.find(query).toArray();
             res.send(products);
+        });
+        app.delete('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) }
+            const result = await productsCollection.deleteOne(filter);
+            res.send(result);
         })
 
 
